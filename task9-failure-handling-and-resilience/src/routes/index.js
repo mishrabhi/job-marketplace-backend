401e7fe470ea95815c6f9c9c6c2dea5b78562c88
Abhishek Mishra <asmyselfabhishek00@gmail.com>
2026-06-28 22:08:35 +0530

    fixed endpoints

--- task9-failure-handling-and-resilience/src/routes/index.js
+++ task9-failure-handling-and-resilience/src/routes/index.js
@@ -1,7 +1,7 @@
 import express from ('express');
 import router from express.Router();
-import failureRoutes from ('./failure.routes');
-import webhookRoutes from ('./webhook.routes');
+import failureRoutes from ('./failure.route.js');
+import webhookRoutes from ('./webhook.route.js');
 
 router.use('/failures', failureRoutes);
 router.use('/webhooks', webhookRoutes);
